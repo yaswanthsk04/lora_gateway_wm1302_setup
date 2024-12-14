1dc091362277f395133f49c39f5b8972c6f05f9f
Yaswanthkumar S K <yaswanthsk04@gmail.com>
2024-12-14 18:47:29 +0100

    v4 automation

--- script.sh
+++ script.sh
@@ -20,6 +20,18 @@ make
 # Navigate to the util_chip_id folder
 cd util_chip_id
 
+# Give execute permissions to reset_lgw.sh in util_chip_id
+chmod +x reset_lgw.sh
+
+# Navigate to packet_forwarder directory from sx1302_hal directory
+cd ../packet_forwarder
+
+# Give execute permissions to reset_lgw.sh in packet_forwarder
+chmod +x reset_lgw.sh
+
+# Return to util_chip_id to run chip_id
+cd ../util_chip_id
+
 # Run the chip_id file and extract the EUI
 output=$(./chip_id)
 eui=$(echo "$output" | grep -oP 'concentrator EUI: \K[0-9a-fA-F]+')
